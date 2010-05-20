17404bfe8f2ee53036f4455e00c1df22518bc001
pitr <pitr.vern@gmail.com>
2010-05-20 00:49:37 -0400

    add yaml configuration for db, etc

--- main.rb
+++ main.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'rack-flash'
+require 'yaml'
+
+configure do
+  enable :sessions
+  use Rack::Flash
+  
+  @config = YAML::load(File.open("config.yaml"))
+end
 
 get '/' do
   "Hello world"
+end
+
+not_found do
+  'Oops, nothing here'
 end
